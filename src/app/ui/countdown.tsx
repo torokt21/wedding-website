bd7755eb88e29b8aeb6638367e66b63acec39339
Tamás Török <torokt21@gmail.com>
2025-05-22 15:28:36 +0200

    Responsive countdown

--- src/app/ui/countdown.tsx
+++ src/app/ui/countdown.tsx
@@ -13,7 +13,7 @@ const ephesis = Ephesis({
 
 export default function Countdown() {
 	const targetDate = new Date(2026, 5, 26, 16, 0, 0).getTime();
-	const [timeLeft, setTimeLeft] = useState<number>(targetDate - Date.now());
+	const [timeLeft, setTimeLeft] = useState<number>(0);
 
 	const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
 	const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -35,42 +35,38 @@ export default function Countdown() {
 	}
 
 	return (
-		<div className="bg-[#e6f0e9] rounded-3xl p-8 max-w-xl mx-auto text-center relative">
+		<div className="bg-[#e6f0e9] rounded-3xl p-8 max-w-2xl mx-auto text-center relative md:pt-8">
+			{/* Small only */}
 			<p
-				className={`${ephesis.className} absolute top-[-1.4rem] left-1/2 transform -translate-x-1/2 text-3xl md:text-4xl text-nowrap`}>
+				className={`${ephesis.className} text-3xl md:text-4xl md:text-nowrap block md:hidden mb-5`}>
 				A Boldogító igenre <span className="hidden md:inline">még</span> ennyit kell
 				várnunk:
 			</p>
-			<div className="flex justify-center gap-8">
-				<div className="bg-primary-400 rounded-3xl py-6 w-25 md:w-32 relative">
-					<p className="text-4xl md:text-4xl font-serif">{days}</p>
-					<p
-						className={`${ephesis.className} text-4xl italic font-[cursive] absolute bottom-[-1.2rem] left-1/2 transform -translate-x-1/2`}>
-						Nap
-					</p>
-				</div>
-				<div className="bg-[#bcd0c0] rounded-3xl px-8 py-6 w-25 md:w-32 relative">
-					<p className="text-4xl md:text-4xl font-serif">{hours}</p>
-					<p
-						className={`${ephesis.className} text-4xl italic font-[cursive] absolute bottom-[-1.2rem] left-1/2 transform -translate-x-1/2`}>
-						Óra
-					</p>
-				</div>
-				<div className="bg-[#bcd0c0] rounded-3xl px-8 py-6 w-25 md:w-32 relative">
-					<p className="text-4xl md:text-4xl font-serif">{minutes}</p>
-					<p
-						className={`${ephesis.className} text-4xl italic font-[cursive] absolute bottom-[-1.2rem] left-1/2 transform -translate-x-1/2`}>
-						Perc
-					</p>
-				</div>
-				<div className="bg-[#bcd0c0] rounded-3xl px-8 py-6 w-32 hidden md:block relative">
-					<p className="text-4xl md:text-4xl font-serif">{seconds}</p>
-					<p
-						className={`${ephesis.className} text-4xl italic font-[cursive] absolute bottom-[-1.2rem] left-1/2 transform -translate-x-1/2`}>
-						Másodperc
-					</p>
-				</div>
+
+			{/* Md up */}
+			<p
+				className={`${ephesis.className} absolute top-[-1.4rem] left-1/2 transform -translate-x-1/2 text-3xl md:text-4xl md:text-nowrap hidden md:block`}>
+				A Boldogító igenre <span className="hidden md:inline">még</span> ennyit kell
+				várnunk:
+			</p>
+			<div className="flex flex-wrap justify-center gap-3 md:gap-6 gap-y-7">
+				<DisplayField value={days} label="Nap" />
+				<DisplayField value={hours} label="Óra" />
+				<DisplayField value={minutes} label="Perc" />
+				<DisplayField value={seconds} label="Másodperc" />
 			</div>
+		</div>
+	);
+}
+
+function DisplayField({ value, label }: { value: number; label: string }) {
+	return (
+		<div className="bg-[#bcd0c0] rounded-3xl py-6 w-25 md:w-32 relative">
+			<p className="text-3xl md:text-3xl font-serif">{value}</p>
+			<p
+				className={`${ephesis.className} text-3xl md:text-4xl absolute bottom-[-1.2rem] left-1/2 transform -translate-x-1/2`}>
+				{label}
+			</p>
 		</div>
 	);
 }
